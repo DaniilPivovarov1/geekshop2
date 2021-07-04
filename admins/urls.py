0ec13ec1a49170056d5e00bc41a8fa0d31663f72
DaniilPivovarov <daniil.pivovarov2010@gmail.com>
2021-07-04 21:13:13 +0300

    added orders in admin

--- admins/urls.py
+++ admins/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from admins.views import index, UserListView, UserUpdateView, UserDeleteView, UserReturnView, UserCreateView
+from admins.views import index, UserListView, UserUpdateView, UserDeleteView, UserReturnView, UserCreateView, OrderListView, OrderUpdateView, OrderDeleteView
 app_name = 'admins'
 
 urlpatterns = [
@@ -10,4 +10,7 @@ urlpatterns = [
     path('users/update/<int:pk>/', UserUpdateView.as_view(), name='admin_users_update'),
     path('users/delete/<int:pk>/', UserDeleteView.as_view(), name='admin_users_delete'),
     path('users/return/<int:pk>/', UserReturnView.as_view(), name='admin_users_return'),
+    path('orders/', OrderListView.as_view(), name='admin_orders'),
+    path('orders/update/<int:pk>/', OrderUpdateView.as_view(), name='admin_orders_update'),
+    path('orders/delete/<int:pk>/', OrderDeleteView.as_view(), name='admin_orders_delete'),
 ]
