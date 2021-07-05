d29b639906acaeda1e77b996f33251e83e0a7cc9
DaniilPivovarov <daniil.pivovarov2010@gmail.com>
2021-07-05 12:34:02 +0300

    added orders ajax

--- orders/views.py
+++ orders/views.py
@@ -46,6 +46,7 @@ class OrderCreate(CreateView):
                 for num, form in enumerate(formset.forms):
                     form.initial['product'] = basket_items[num].product
                     form.initial['quantity'] = basket_items[num].quantity
+                    form.initial['price'] = basket_items[num].product.price
                 basket_items.delete()
             else:
                 formset = OrderFormSet()
@@ -82,7 +83,11 @@ class OrderUpdate(UpdateView):
         if self.request.POST:
             data['orderitems'] = OrderFormSet(self.request.POST, instance=self.object)
         else:
-            data['orderitems'] = OrderFormSet(instance=self.object)
+            formset = OrderFormSet(instance=self.object)
+            for form in formset.forms:
+                if form.instance.pk:
+                    form.initial['price'] = form.instance.product.price
+            data['orderitems'] = formset
         data['title'] = 'заказ/обновление'
 
         return data
---
+++ static/js/order.js
@@ -0,0 +1,64 @@
+let _quantity, _price, orderitem_num, delta_quantity, orderitem_quantity, delta_cost;
+let quantity_arr = [];
+let price_arr = [];
+
+let TOTAL_FORMS = parseInt(document.querySelector('input[name="orderitems-TOTAL_FORMS"]').value);
+
+let order_total_quantity = parseInt(document.querySelector('.order_total_quantity').textContent) || 0;
+let order_total_cost = parseFloat(document.querySelector('.order_total_cost').textContent.replace(',', '.')) || 0;
+
+for (let i = 0; i < TOTAL_FORMS-1; i++) {
+    _quantity = parseInt(document.querySelector('input[name="orderitems-' + i + '-quantity"]').value);
+    _price = parseFloat(document.querySelector('.orderitems-' + i + '-price').textContent.replace(',', '.'));
+    quantity_arr[i] = _quantity;
+    if (_price) {
+        price_arr[i] = _price;
+    } else {
+        price_arr[i] = 0;
+    }
+}
+if (!order_total_quantity) {
+    for (let i = 0; i < TOTAL_FORMS-1; i++) {
+        order_total_quantity += quantity_arr[i];
+        order_total_cost += quantity_arr[i] * price_arr[i];
+    }
+    document.querySelector('.order_total_quantity').innerHTML = order_total_quantity.toString();
+    document.querySelector('.order_total_cost').innerHTML = Number(order_total_cost.toFixed(2)).toString();
+}
+
+document.querySelector('.order_form').querySelectorAll('input[type="number"]').forEach(function (inp) {
+    inp.addEventListener('click', function (event) {
+        let target = event.target;
+        orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-quantity', ''));
+        if (price_arr[orderitem_num]) {
+            orderitem_quantity = parseInt(target.value);
+            delta_quantity = orderitem_quantity - quantity_arr[orderitem_num];
+            quantity_arr[orderitem_num] = orderitem_quantity;
+            orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
+        }
+    })
+});
+
+document.querySelector('.order_form').querySelectorAll('input[type="checkbox"]').forEach(function (inp) {
+    inp.addEventListener('click', function (event) {
+        let target = event.target;
+        orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-DELETE', ''));
+        if (target.checked) {
+            delta_quantity = -quantity_arr[orderitem_num];
+        } else {
+            delta_quantity = quantity_arr[orderitem_num];
+        }
+        orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
+    })
+});
+
+
+function orderSummaryUpdate(orderitem_price, delta_quantity) {
+    delta_cost = orderitem_price * delta_quantity;
+
+    order_total_cost = Number((order_total_cost + delta_cost).toFixed(2));
+    order_total_quantity = order_total_quantity + delta_quantity;
+
+    document.querySelector('.order_total_cost').innerHTML = order_total_cost.toString();
+    document.querySelector('.order_total_quantity').innerHTML = order_total_quantity.toString();
+}
