470bf2d728b8605cf5a44f52ff8c92c5964031d5
DaniilPivovarov <daniil.pivovarov2010@gmail.com>
2021-07-15 15:32:51 +0300

    added caching in template

--- orders/views.py
+++ orders/views.py
@@ -75,7 +75,7 @@ class OrderCreate(CreateView):
                 orderitems.instance = self.object
                 orderitems.save()
 
-        if self.object.get_total_cost() == 0:
+        if self.object.get_summary()['total_cost'] == 0:
             self.object.delete()
 
         return super(OrderCreate, self).form_valid(form)
@@ -116,7 +116,7 @@ class OrderUpdate(UpdateView):
                 orderitems.instance = self.object
                 orderitems.save()
 
-        if self.object.get_total_cost() == 0:
+        if self.object.get_summary()['total_cost'] == 0:
             self.object.delete()
 
         return super(OrderUpdate, self).form_valid(form)
@@ -156,7 +156,7 @@ def forming_complete(request, pk):
 @receiver(pre_save, sender=Basket)
 def product_quantity_update_save(sender, update_fields, instance, **kwargs):
     if instance.pk:
-        instance.product.quantity -= instance.quantity - sender.get_item(instance.pk).quantity
+        instance.product.quantity -= instance.quantity - sender.objects.get(pk=instance.pk).quantity
     else:
         instance.product.quantity -= instance.quantity
     instance.product.save()
